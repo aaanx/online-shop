72025434b7de457a91e92cfc046018553b1a74c6
aaanx <anna.walkowiakk@gmail.com>
2019-06-15 11:26:01 +0200

    Add products data

---
+++ src/productsData.js
@@ -0,0 +1,142 @@
+export default [
+  {
+    id: 19,
+    title: "Browar Fortuna",
+    price: 12.99,
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae rutrum arcu. Ut sit amet feugiat est, at faucibus quam. Donec eget justo ac ipsum convallis porttitor. In a pellentesque ante, nec rhoncus nunc. Nam id ante placerat, malesuada lacus eget, pellentesque turpis. Nulla vestibulum dolor sit amet rutrum fermentum. Cras fringilla risus vitae ligula gravida, quis maximus nibh sagittis. Nulla massa sem, venenatis eget bibendum ut, ullamcorper id lacus. Praesent tortor massa, feugiat vel magna tempor, lacinia varius massa. Vivamus facilisis non justo eget ullamcorper. In at ultrices odio. Maecenas id egestas risus, sed laoreet nisi. Aenean lacinia dolor vitae leo posuere sodales. Mauris et justo diam. Fusce ornare auctor ante consectetur mollis. Aliquam sagittis sed sapien eget scelerisque.Nam sit amet varius odio, non dignissim ante.Vestibulum facilisis leo id enim cursus dignissim.Integer id feugiat elit, sed fringilla ligula.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Quisque nunc nisi, gravida et ex quis, mollis ornare augue.Aenean varius, felis in pretium ultrices, libero tellus efficitur elit, ut lacinia urna dolor sit amet urna.Maecenas eu purus vel ex dapibus vestibulum.Praesent eleifend felis non ante hendrerit congue at at purus.Fusce viverra lobortis turpis, non gravida velit ultrices eget.    Mauris efficitur vulputate lacus, viverra cursus nunc porta eget.Ut quis tellus eu lorem pharetra faucibus eu quis purus.Maecenas imperdiet, lorem non luctus luctus, elit metus tempus metus, nec consequat velit turpis ac diam.Vivamus eget nisi blandit, aliquam tortor id, posuere nisl.Sed tincidunt condimentum neque.Vestibulum varius, odio at pulvinar dapibus, massa lectus fermentum massa, vel tempor ante leo vitae enim.Integer lectus mauris, ultricies quis nunc tincidunt, lobortis imperdiet eros.Nam tempus, urna eu varius mattis, sem elit rhoncus sapien, non interdum diam massa et odio.Phasellus porttitor nulla eget mi euismod porttitor.Suspendisse consequat magna vel efficitur egestas. Vestibulum ullamcorper laoreet mauris, eu scelerisque metus imperdiet nec.Nulla consequat euismod diam eu mollis.Pellentesque lacus lacus, pharetra ullamcorper bibendum non, mattis eget quam.Pellentesque non ligula at turpis finibus tincidunt vitae et augue.Cras id libero neque.Quisque venenatis in ex sit amet placerat.Cras orci metus, vestibulum non vestibulum sed, euismod non sapien.Nullam ac feugiat eros.Pellentesque vel mauris at est commodo euismod.Suspendisse faucibus iaculis aliquam.Pellentesque vitae elit non turpis eleifend iaculis eu nec sem.",
+    text2: "",
+    shortDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    socialDescription: null,
+    seoDescription: "browar, picie, alko, impreza, grill",
+    createdAt: "2018-02-13T14:15:33.000Z",
+    updatedAt: "2018-12-28T16:43:15.000Z",
+    imageId: 6,
+    image: {
+      id: 6,
+      name: "54b8353ca5a58a255c5e6fd99e1473ac.jpg",
+      description: null,
+      url:
+        "https://images.unsplash.com/photo-1474225183201-99971f7495cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      width: 1262,
+      height: 768,
+      size: "74740",
+      createdAt: "2018-12-28T16:43:12.000Z",
+      updatedAt: "2018-12-28T16:43:12.000Z"
+    },
+    tags: ["browar", "picie", "alko", "impreza", "grill"]
+  },
+  {
+    id: 20,
+    title: "Browar Lech",
+    price: 11.99,
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae rutrum arcu. Ut sit amet feugiat est, at faucibus quam. Donec eget justo ac ipsum convallis porttitor. In a pellentesque ante, nec rhoncus nunc. Nam id ante placerat, malesuada lacus eget, pellentesque turpis. Nulla vestibulum dolor sit amet rutrum fermentum. Cras fringilla risus vitae ligula gravida, quis maximus nibh sagittis. Nulla massa sem, venenatis eget bibendum ut, ullamcorper id lacus. Praesent tortor massa, feugiat vel magna tempor, lacinia varius massa. Vivamus facilisis non justo eget ullamcorper. In at ultrices odio. Maecenas id egestas risus, sed laoreet nisi. Aenean lacinia dolor vitae leo posuere sodales. Mauris et justo diam. Fusce ornare auctor ante consectetur mollis. Aliquam sagittis sed sapien eget scelerisque.Nam sit amet varius odio, non dignissim ante.Vestibulum facilisis leo id enim cursus dignissim.Integer id feugiat elit, sed fringilla ligula.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Quisque nunc nisi, gravida et ex quis, mollis ornare augue.Aenean varius, felis in pretium ultrices, libero tellus efficitur elit, ut lacinia urna dolor sit amet urna.Maecenas eu purus vel ex dapibus vestibulum.Praesent eleifend felis non ante hendrerit congue at at purus.Fusce viverra lobortis turpis, non gravida velit ultrices eget.    Mauris efficitur vulputate lacus, viverra cursus nunc porta eget.Ut quis tellus eu lorem pharetra faucibus eu quis purus.Maecenas imperdiet, lorem non luctus luctus, elit metus tempus metus, nec consequat velit turpis ac diam.Vivamus eget nisi blandit, aliquam tortor id, posuere nisl.Sed tincidunt condimentum neque.Vestibulum varius, odio at pulvinar dapibus, massa lectus fermentum massa, vel tempor ante leo vitae enim.Integer lectus mauris, ultricies quis nunc tincidunt, lobortis imperdiet eros.Nam tempus, urna eu varius mattis, sem elit rhoncus sapien, non interdum diam massa et odio.Phasellus porttitor nulla eget mi euismod porttitor.Suspendisse consequat magna vel efficitur egestas. Vestibulum ullamcorper laoreet mauris, eu scelerisque metus imperdiet nec.Nulla consequat euismod diam eu mollis.Pellentesque lacus lacus, pharetra ullamcorper bibendum non, mattis eget quam.Pellentesque non ligula at turpis finibus tincidunt vitae et augue.Cras id libero neque.Quisque venenatis in ex sit amet placerat.Cras orci metus, vestibulum non vestibulum sed, euismod non sapien.Nullam ac feugiat eros.Pellentesque vel mauris at est commodo euismod.Suspendisse faucibus iaculis aliquam.Pellentesque vitae elit non turpis eleifend iaculis eu nec sem.",
+    text2: "",
+    shortDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    socialDescription: null,
+    seoDescription: "browar, picie, alko, impreza, grill",
+    createdAt: "2018-02-13T14:15:33.000Z",
+    updatedAt: "2018-12-28T16:43:15.000Z",
+    imageId: 6,
+    image: {
+      id: 6,
+      name: "54b8353ca5a58a255c5e6fd99e1473ac.jpg",
+      description: null,
+      url:
+        "https://images.unsplash.com/photo-1474225183201-99971f7495cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      width: 1262,
+      height: 768,
+      size: "74740",
+      createdAt: "2018-12-28T16:43:12.000Z",
+      updatedAt: "2018-12-28T16:43:12.000Z"
+    },
+    tags: ["browar", "picie", "alko", "impreza", "grill"]
+  },
+  {
+    id: 21,
+    title: "Browar Tyskie",
+    price: 9.99,
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae rutrum arcu. Ut sit amet feugiat est, at faucibus quam. Donec eget justo ac ipsum convallis porttitor. In a pellentesque ante, nec rhoncus nunc. Nam id ante placerat, malesuada lacus eget, pellentesque turpis. Nulla vestibulum dolor sit amet rutrum fermentum. Cras fringilla risus vitae ligula gravida, quis maximus nibh sagittis. Nulla massa sem, venenatis eget bibendum ut, ullamcorper id lacus. Praesent tortor massa, feugiat vel magna tempor, lacinia varius massa. Vivamus facilisis non justo eget ullamcorper. In at ultrices odio. Maecenas id egestas risus, sed laoreet nisi. Aenean lacinia dolor vitae leo posuere sodales. Mauris et justo diam. Fusce ornare auctor ante consectetur mollis. Aliquam sagittis sed sapien eget scelerisque.Nam sit amet varius odio, non dignissim ante.Vestibulum facilisis leo id enim cursus dignissim.Integer id feugiat elit, sed fringilla ligula.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Quisque nunc nisi, gravida et ex quis, mollis ornare augue.Aenean varius, felis in pretium ultrices, libero tellus efficitur elit, ut lacinia urna dolor sit amet urna.Maecenas eu purus vel ex dapibus vestibulum.Praesent eleifend felis non ante hendrerit congue at at purus.Fusce viverra lobortis turpis, non gravida velit ultrices eget.    Mauris efficitur vulputate lacus, viverra cursus nunc porta eget.Ut quis tellus eu lorem pharetra faucibus eu quis purus.Maecenas imperdiet, lorem non luctus luctus, elit metus tempus metus, nec consequat velit turpis ac diam.Vivamus eget nisi blandit, aliquam tortor id, posuere nisl.Sed tincidunt condimentum neque.Vestibulum varius, odio at pulvinar dapibus, massa lectus fermentum massa, vel tempor ante leo vitae enim.Integer lectus mauris, ultricies quis nunc tincidunt, lobortis imperdiet eros.Nam tempus, urna eu varius mattis, sem elit rhoncus sapien, non interdum diam massa et odio.Phasellus porttitor nulla eget mi euismod porttitor.Suspendisse consequat magna vel efficitur egestas. Vestibulum ullamcorper laoreet mauris, eu scelerisque metus imperdiet nec.Nulla consequat euismod diam eu mollis.Pellentesque lacus lacus, pharetra ullamcorper bibendum non, mattis eget quam.Pellentesque non ligula at turpis finibus tincidunt vitae et augue.Cras id libero neque.Quisque venenatis in ex sit amet placerat.Cras orci metus, vestibulum non vestibulum sed, euismod non sapien.Nullam ac feugiat eros.Pellentesque vel mauris at est commodo euismod.Suspendisse faucibus iaculis aliquam.Pellentesque vitae elit non turpis eleifend iaculis eu nec sem.",
+    text2: "",
+    shortDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    socialDescription: null,
+    seoDescription: "browar, picie, alko, impreza, grill",
+    createdAt: "2018-02-13T14:15:33.000Z",
+    updatedAt: "2018-12-28T16:43:15.000Z",
+    imageId: 6,
+    image: {
+      id: 6,
+      name: "54b8353ca5a58a255c5e6fd99e1473ac.jpg",
+      description: null,
+      url:
+        "https://images.unsplash.com/photo-1474225183201-99971f7495cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      width: 1262,
+      height: 768,
+      size: "74740",
+      createdAt: "2018-12-28T16:43:12.000Z",
+      updatedAt: "2018-12-28T16:43:12.000Z"
+    },
+    tags: ["browar", "picie", "alko", "impreza", "grill"]
+  },
+  {
+    id: 22,
+    title: "Browar Miłosław",
+    price: 15.99,
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae rutrum arcu. Ut sit amet feugiat est, at faucibus quam. Donec eget justo ac ipsum convallis porttitor. In a pellentesque ante, nec rhoncus nunc. Nam id ante placerat, malesuada lacus eget, pellentesque turpis. Nulla vestibulum dolor sit amet rutrum fermentum. Cras fringilla risus vitae ligula gravida, quis maximus nibh sagittis. Nulla massa sem, venenatis eget bibendum ut, ullamcorper id lacus. Praesent tortor massa, feugiat vel magna tempor, lacinia varius massa. Vivamus facilisis non justo eget ullamcorper. In at ultrices odio. Maecenas id egestas risus, sed laoreet nisi. Aenean lacinia dolor vitae leo posuere sodales. Mauris et justo diam. Fusce ornare auctor ante consectetur mollis. Aliquam sagittis sed sapien eget scelerisque.Nam sit amet varius odio, non dignissim ante.Vestibulum facilisis leo id enim cursus dignissim.Integer id feugiat elit, sed fringilla ligula.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Quisque nunc nisi, gravida et ex quis, mollis ornare augue.Aenean varius, felis in pretium ultrices, libero tellus efficitur elit, ut lacinia urna dolor sit amet urna.Maecenas eu purus vel ex dapibus vestibulum.Praesent eleifend felis non ante hendrerit congue at at purus.Fusce viverra lobortis turpis, non gravida velit ultrices eget.    Mauris efficitur vulputate lacus, viverra cursus nunc porta eget.Ut quis tellus eu lorem pharetra faucibus eu quis purus.Maecenas imperdiet, lorem non luctus luctus, elit metus tempus metus, nec consequat velit turpis ac diam.Vivamus eget nisi blandit, aliquam tortor id, posuere nisl.Sed tincidunt condimentum neque.Vestibulum varius, odio at pulvinar dapibus, massa lectus fermentum massa, vel tempor ante leo vitae enim.Integer lectus mauris, ultricies quis nunc tincidunt, lobortis imperdiet eros.Nam tempus, urna eu varius mattis, sem elit rhoncus sapien, non interdum diam massa et odio.Phasellus porttitor nulla eget mi euismod porttitor.Suspendisse consequat magna vel efficitur egestas. Vestibulum ullamcorper laoreet mauris, eu scelerisque metus imperdiet nec.Nulla consequat euismod diam eu mollis.Pellentesque lacus lacus, pharetra ullamcorper bibendum non, mattis eget quam.Pellentesque non ligula at turpis finibus tincidunt vitae et augue.Cras id libero neque.Quisque venenatis in ex sit amet placerat.Cras orci metus, vestibulum non vestibulum sed, euismod non sapien.Nullam ac feugiat eros.Pellentesque vel mauris at est commodo euismod.Suspendisse faucibus iaculis aliquam.Pellentesque vitae elit non turpis eleifend iaculis eu nec sem.",
+    text2: "",
+    shortDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    socialDescription: null,
+    seoDescription: "browar, picie, alko, impreza, grill",
+    createdAt: "2018-02-13T14:15:33.000Z",
+    updatedAt: "2018-12-28T16:43:15.000Z",
+    imageId: 6,
+    image: {
+      id: 6,
+      name: "54b8353ca5a58a255c5e6fd99e1473ac.jpg",
+      description: null,
+      url:
+        "https://images.unsplash.com/photo-1474225183201-99971f7495cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      width: 1262,
+      height: 768,
+      size: "74740",
+      createdAt: "2018-12-28T16:43:12.000Z",
+      updatedAt: "2018-12-28T16:43:12.000Z"
+    },
+    tags: ["browar", "picie", "alko", "impreza", "grill"]
+  },
+  {
+    id: 23,
+    title: "Browar Żubr",
+    price: 2.99,
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vitae rutrum arcu. Ut sit amet feugiat est, at faucibus quam. Donec eget justo ac ipsum convallis porttitor. In a pellentesque ante, nec rhoncus nunc. Nam id ante placerat, malesuada lacus eget, pellentesque turpis. Nulla vestibulum dolor sit amet rutrum fermentum. Cras fringilla risus vitae ligula gravida, quis maximus nibh sagittis. Nulla massa sem, venenatis eget bibendum ut, ullamcorper id lacus. Praesent tortor massa, feugiat vel magna tempor, lacinia varius massa. Vivamus facilisis non justo eget ullamcorper. In at ultrices odio. Maecenas id egestas risus, sed laoreet nisi. Aenean lacinia dolor vitae leo posuere sodales. Mauris et justo diam. Fusce ornare auctor ante consectetur mollis. Aliquam sagittis sed sapien eget scelerisque.Nam sit amet varius odio, non dignissim ante.Vestibulum facilisis leo id enim cursus dignissim.Integer id feugiat elit, sed fringilla ligula.Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Quisque nunc nisi, gravida et ex quis, mollis ornare augue.Aenean varius, felis in pretium ultrices, libero tellus efficitur elit, ut lacinia urna dolor sit amet urna.Maecenas eu purus vel ex dapibus vestibulum.Praesent eleifend felis non ante hendrerit congue at at purus.Fusce viverra lobortis turpis, non gravida velit ultrices eget.    Mauris efficitur vulputate lacus, viverra cursus nunc porta eget.Ut quis tellus eu lorem pharetra faucibus eu quis purus.Maecenas imperdiet, lorem non luctus luctus, elit metus tempus metus, nec consequat velit turpis ac diam.Vivamus eget nisi blandit, aliquam tortor id, posuere nisl.Sed tincidunt condimentum neque.Vestibulum varius, odio at pulvinar dapibus, massa lectus fermentum massa, vel tempor ante leo vitae enim.Integer lectus mauris, ultricies quis nunc tincidunt, lobortis imperdiet eros.Nam tempus, urna eu varius mattis, sem elit rhoncus sapien, non interdum diam massa et odio.Phasellus porttitor nulla eget mi euismod porttitor.Suspendisse consequat magna vel efficitur egestas. Vestibulum ullamcorper laoreet mauris, eu scelerisque metus imperdiet nec.Nulla consequat euismod diam eu mollis.Pellentesque lacus lacus, pharetra ullamcorper bibendum non, mattis eget quam.Pellentesque non ligula at turpis finibus tincidunt vitae et augue.Cras id libero neque.Quisque venenatis in ex sit amet placerat.Cras orci metus, vestibulum non vestibulum sed, euismod non sapien.Nullam ac feugiat eros.Pellentesque vel mauris at est commodo euismod.Suspendisse faucibus iaculis aliquam.Pellentesque vitae elit non turpis eleifend iaculis eu nec sem.",
+    text2: "",
+    shortDescription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    socialDescription: null,
+    seoDescription: "browar, picie, alko, impreza, grill",
+    createdAt: "2018-02-13T14:15:33.000Z",
+    updatedAt: "2018-12-28T16:43:15.000Z",
+    imageId: 6,
+    image: {
+      id: 6,
+      name: "54b8353ca5a58a255c5e6fd99e1473ac.jpg",
+      description: null,
+      url:
+        "https://images.unsplash.com/photo-1474225183201-99971f7495cf?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+      width: 1262,
+      height: 768,
+      size: "74740",
+      createdAt: "2018-12-28T16:43:12.000Z",
+      updatedAt: "2018-12-28T16:43:12.000Z"
+    },
+    tags: ["browar", "picie", "alko", "impreza", "grill"]
+  }
+];
