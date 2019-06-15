34672287df50adcd28c8a357ff0a8bd3ad5a00ef
Ewa <ewsekowska@gmail.com>
2019-06-15 14:23:51 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Category from './Pages/CategoryPage.js';
+import productPage from './Pages/ProductPage.js';
 
 import './App.css';
 
 function App() {
   return (
-    <Category />
+    <Router>
+      <>
+        <Category />
+
+        <Route exact path="/:id" component={productPage} />
+      </>
+    </Router>
   );
 }
 
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './productBox.css';
+
 
 function ProductBox(props) {
     return (
+
         <div className="col-lg-4 col-md-4 col-sm-12 box">
-            <div class="boxContent">
+            <div className="boxContent">
                 <img src={props.image.url} />
-                <p className="productTitle">{props.title}</p>
+                <Link to={`/${props.id}`}><p className="productTitle">{props.title}</p></Link>
                 <p className="productPrice">{props.price} zł</p>
             </div>
         </div>
+
     )
 }
 
