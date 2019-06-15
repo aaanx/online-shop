f9fe4ff16de35397f7f69fea1356005740926bd7
unknown <karczynskijakub1@gmail.com>
2019-06-15 17:53:36 +0200

    kolejny commit

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,11 +1,19 @@
 import React from "react";
-import FaBeer from 'react-icons/lib/fa/beer';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "...\node_modules\react";
+import FaBeer from "react-icons/lib/fa/bear";
 class Header extends React.Component {
   render() {
     return (
       <div style={{ paddingLeft: "97%" }}>
-        <button  onClick={this.basket}><Router><link to="/Pages/BasktePages/"><FaBeer /></link></Router></button>
+        <Router>
+          <button onClick={this.basket}>
+            <Link to="/BasketPage/">
+              <FaBear />
+            </Link>
+            <Route exact path="/BasketPage" component={Basket} />
+          </button>
+        </Router>
+        <button onClick={this.counter}>Licznik</button>
       </div>
     );
   }
