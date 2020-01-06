afb549d3089ff8c00ae2ff98e982f41cc5dc3f99
aaanx <anna.walkowiakk@gmail.com>
2020-01-06 14:03:55 +0100

    Limit num of products per page

--- src/Pages/CategoryPage.js
+++ src/Pages/CategoryPage.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Products from "../productsData.js";
 import Product from "../Components/Product/Product.js";
 import Pagination from "../Components/Pagination/Pagination.js";
 
 function Category(props) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage, setProductsPerPage] = useState(6);
+
+  const products = { Products };
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = Object.values(products)[0].slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
   return (
     <div className="container">
       <div className="row">
-        {Products.map(product => {
+        {currentProducts.map(product => {
           return (
             <Product
               key={product.id}
