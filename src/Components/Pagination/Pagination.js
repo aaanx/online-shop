4b7ad626aca38b179241ff2e0464d8657e2692e6
aaanx <anna.walkowiakk@gmail.com>
2020-01-06 15:51:25 +0100

    Add pagination

--- src/Components/Pagination/Pagination.js
+++ src/Components/Pagination/Pagination.js
@@ -1,18 +1,39 @@
 import React from "react";
 import "./Pagination.css";
 
-function Pagination(props) {
+const Pagination = ({
+  productsPerPage,
+  totalProducts,
+  paginate,
+  currentPage
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  console.log(currentPage);
+
   return (
     <div>
       <div className="pagination">
-        <span>&laquo;</span>
-        <span className="active">1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
+        {pageNumbers.map(pageNum => {
+          let classes =
+            currentPage === pageNum ? "page-item-active" : "page-item";
+          return (
+            <span key={pageNum} className={classes}>
+              <a className="p-link" onClick={() => paginate(pageNum)}>
+                {pageNum}
+              </a>
+            </span>
+          );
+        })}
+
+        {/* <span>&laquo;</span> */}
       </div>
     </div>
   );
-}
+};
 
 export default Pagination;
--- src/Pages/CategoryPage.js
+++ src/Pages/CategoryPage.js
@@ -5,16 +5,20 @@ import Pagination from "../Components/Pagination/Pagination.js";
 
 function Category(props) {
   const [currentPage, setCurrentPage] = useState(1);
-  const [productsPerPage, setProductsPerPage] = useState(6);
+  const [productsPerPage] = useState(6);
 
   const products = { Products };
 
+  // get current products
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
   const currentProducts = Object.values(products)[0].slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
+
+  // change page
+  const paginate = pageNum => setCurrentPage(pageNum);
 
   return (
     <div className="container">
@@ -29,7 +33,12 @@ function Category(props) {
           );
         })}
       </div>
-      <Pagination />
+      <Pagination
+        productsPerPage={productsPerPage}
+        totalProducts={Object.values(products)[0].length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </div>
   );
 }
